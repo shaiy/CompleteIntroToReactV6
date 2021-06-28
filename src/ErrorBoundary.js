5bcfec27465181413db4fccb9046d40c99591685
Shai Yomtov <shaiy@liveperson.com>
2021-06-28 11:55:19 +0300

    Redirect on error

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,21 +1,24 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     //Log to logger (like loggly \ NewRelic)
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    return this.state.hasError ? (
+    return this.state.redirect ? (
+      <Redirect to="/" />
+    ) : this.state.hasError ? (
       <h2>
         This listing has an error. <Link to="/">Click here</Link> to go back to
-        the home page.
+        the home page or wait five seconds.
       </h2>
     ) : (
       this.props.children
